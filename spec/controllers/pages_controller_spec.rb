66cc67c9ffd728868793f27ddbd1536b8e46511f
Tom Porter <tporter@eclipse-bb.(none)>
2011-02-07 10:11:09 -0500

    New Branch with spork/autotest setu from SS

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,4 +16,18 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'links'" do
+    it "should be successful" do
+      get 'links'
+      response.should be_success
+    end
+  end
+
 end
