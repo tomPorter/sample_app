6fdaea8698ba82393fd914d2a0afd10325f0dfd8
Tom Porter <thomas.porter@acm.org>
2011-02-07 20:01:27 -0500

    Cleaned up README, added notes on weirdnesses so far.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,6 +12,10 @@ describe PagesController do
       get 'home'
       response.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Home")
     end
+    it "should have non-blank  body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 
   describe "GET 'contact'" do
@@ -19,11 +23,19 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
